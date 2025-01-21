deaafb5fbeb32f83938be23213ed3676085390a0
Kevin Jimenez <KevinJG994@gmail.com>
2025-01-21 12:41:56 +0000

    [Add] Filter and Reduce method

--- src/quiz.js
+++ src/quiz.js
@@ -1,34 +1,50 @@
 class Quiz {
-    constructor(questions, timeLimit, timeRemaining) {
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers = 0;
-        this.currentQuestionIndex = 0;
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+
+  shuffleQuestions() {
+    this.questions.sort(() => Math.random() - 1);
+  }
+
+  checkAnswer(answer) {
+    this.correctAnswers++;
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
     }
+  }
 
-    getQuestion() {
-        return this.questions[this.currentQuestionIndex];
-    };
-
-
-    moveToNextQuestion() {
-        this.currentQuestionIndex++;
+  filterQuestionsByDifficulty(difficulty) {
+    if (difficulty >= 1 && difficulty <= 3) {
+      this.questions = this.questions.filter(
+        (array) => array.difficulty === difficulty
+      );
     }
+  }
 
-    shuffleQuestions() {
-        this.questions.sort(() => Math.random() - 1)
-    }
+  averageDifficulty() {
+    const totalDifficulty = this.questions.reduce(
+      (acc, questionDifficulty) => acc + questionDifficulty.difficulty,
+      0
+    );
 
-    checkAnswer(answer) {
-        this.correctAnswers++;
-    }
-
-    hasEnded() {
-        if (this.currentQuestionIndex < this.questions.length) {
-            return false;
-        } else if (this.currentQuestionIndex === this.questions.length) {
-            return true;
-        }
-    }
+    return totalDifficulty / this.questions.length;
+  }
 }
